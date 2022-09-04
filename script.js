9bde54a441608200115efcd3d3294351ef3c68a6
CatalinaGoldsack <cgoldsack@uc.cl>
2022-09-04 03:45:16 -0300

    Mapa cargado

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+  var map = L.map("map").setView([51.505, -0.09], 13);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+  }).addTo(map);
+};
