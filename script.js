cef78a8bf5630c5ee9bd65188a30726b54ce44a3
CatalinaGoldsack <cgoldsack@uc.cl>
2022-09-06 14:55:19 -0400

    Agregado nombre y fecha a mensajes del chat

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ function uuidv4() {
     ).toString(16)
   );
 }
+
+// function randomName() {
+//   const names = ["Catalina", "Francisca", "Rafael", "Bernardita", "Mane", "Josefina", "Eugenio"];
+//   const random = Math.floor(Math.random() * names.length);
+//   return names[random]
+// }
 //
 
 function renderBaseMap() {
@@ -24,14 +30,18 @@ function renderBaseMap() {
   }).addTo(map);
 }
 
-
-function insertMessage(text, classes='bg-cyan-600') {
+function insertMessage(text, classes = "bg-cyan-600", name, date) {
   const containerChat = document.getElementById("chat-container");
   containerChat.insertAdjacentHTML(
     "beforeend",
-    `<p class="p-2 rounded text-white ${classes}">
+    // <div class="rounded border-solid border-cyan-800">
+    `<h1 class="font-mono font-semibold text-cyan-800 pt-3"> <i class="fas fa-user p-1"></i> ${name}</h1>
+    <h1 class="text-zinc-700 text-xs"> ${date}</h1>
+    <p class="p-2 rounded text-white ${classes}">
             ${text}
-        </p>
+            
+      </p>
+   
         `
   );
 }
@@ -41,11 +51,11 @@ function onChatSend() {
   const text = textArea.value;
   if (text === "") return;
   textArea.value = "";
-  
+
   sendObject({
-    type: 'chat',
-    content: text
-  })
+    type: "chat",
+    content: text,
+  });
 }
 
 function onChatKeypress(event) {
@@ -70,55 +80,54 @@ function sendObject(object) {
 }
 
 function onMessageReceived(object) {
-  const classes = object.message.type === 'warn' ? 'bg-red-500' : 'bg-cyan-600'
+  const classes = object.message.type === "warn" ? "bg-red-500" : "bg-cyan-600";
 
-  insertMessage(object.message.content, classes);
+  insertMessage(
+    object.message.content,
+    classes,
+    object.message.name,
+    object.message.date
+  );
   const containerChat = document.getElementById("chat-scroll-container");
   containerChat.scrollTop = containerChat.scrollHeight;
 }
 
-function onCrashedReceived(object) {
+function onCrashedReceived(object) {}
 
-}
+function onLandingReceived(object) {}
 
-function onLandingReceived(object) {
+function onTakeOffReceived(object) {}
 
-}
+function onPlaneReceived(object) {}
 
-function onTakeOffReceived(object) {
-
-}
-
-function onPlaneReceived(object) {
-
-}
-
-function onFlightsReceived(object) {
-  
-}
+function onFlightsReceived(object) {}
 
 function onObjectReceived(event) {
-  const object = JSON.parse(event.data)
+  const object = JSON.parse(event.data);
 
-  switch(object.type) {
-    case 'message':
+  switch (object.type) {
+    case "message":
+      onMessageReceived(object);
+      break;
+    case "flights":
       onMessageReceived(object)
-      break
+      break;
   }
 }
 
 function setupWebsocket() {
   webSocket = new WebSocket(
     "wss://tarea-1.2022-2.tallerdeintegracion.cl/connect"
-  )
+  );
   webSocket.onopen = () => {
     sendObject({
       type: "join",
       id: uuidv4(),
-    })
-  }
+      username: "Catalina",
+    });
+  };
 
-  webSocket.addEventListener('message', onObjectReceived)
+  webSocket.addEventListener("message", onObjectReceived);
 }
 
 window.onload = () => {
@@ -127,12 +136,11 @@ window.onload = () => {
   setupWebsocket();
 };
 
-
 // let count = 0;
-  // setInterval(() => {
-  //   count++;
-  //   insertMessage(count);
-  // }, 1000);
+// setInterval(() => {
+//   count++;
+//   insertMessage(count);
+// }, 1000);
 
 // setTimeout(() => {
 //     const textArea = document.getElementById('chat-input')
